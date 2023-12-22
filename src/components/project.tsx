59db16d50f33d5d00cc43b2856523ea89f7097b0
ALostCAWs <keighly@keighly.ca>
2023-12-22 16:20:43 -0500

    Adds project section & fixes to CSS

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,32 +1,38 @@
 interface ProjectProps {
   title: string,
+  link: string,
   date: string,
   skills: string[],
-  description: string,
-  image: string
+  description: string[],
+  image: React.ReactNode
 }
 
-export const Project = ({ title, date, skills, description, image }: ProjectProps) => {
-  const link = '';
+export const Project = ({ title, link, date, skills, description, image }: ProjectProps) => {
   const skillList = skills.join(', ');
+  const imgSrc = `assets/thumbnails/${image}.png`;
+  console.log(imgSrc)
   return (
     <>
-      <article>
+      <article className='project'>
         <div className='thumb'>
-          <a href={link}>
+          <a href={link} target='_blank'>
             <div className='overlay'>
               <div className='thumb-txt'>{title}</div>
             </div>
-            <img src={`../assets/${image}.png`} alt={`${title} thumbnail image`} />
+            {image}
           </a>
         </div>
         <div className='info-container'>
-          <h3><a href={link}>{title}</a></h3>
-          <div>
-            <h4>{date}</h4>
-            <h4>{skillList}</h4>
+          <h3><a href={link} target='_blank'>{title}</a></h3>
+          <div className='date-skills-container'>
+            <p>{date}</p>
+            <p>{skillList}</p>
           </div>
-          <p>{description}</p>
+        </div>
+        <div className='desc-container'>
+          {description.map((item) =>
+            <p>{item}</p>
+          )}
         </div>
       </article>
     </>
