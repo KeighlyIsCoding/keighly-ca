080e11d1ff4392ca70bbfacc342ca701defcfad3
ALostCAWs <keighly@keighly.ca>
2023-12-26 14:00:29 -0500

    Minor updates & font size adjustment

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,8 +4,8 @@ export const Intro = () => {
       <h1>Keighly Hill,</h1>
       <h2>Software Developer</h2>
       <h3>keighly@keighly.ca</h3>
-      <p>I am currently employed as a web developer; with an education spanning the full stack of software development</p>
-      <p>Currently focused on expanding my programming capabilities with TypeScript, React, Node JS, & Dynamo DB to build a full-stack project</p>
+      <p>Web developer with an education spanning the full stack of software development.</p>
+      <p>Currently focused on expanding my knowledge with TypeScript, React, Node JS, & Dynamo DB to build a full-stack project.</p>
     </article>
   );
 }
